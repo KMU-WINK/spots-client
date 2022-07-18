7f138406df43d6473761d8d201a4cebfa481199d
j.myeong_ <jmyeong012@gmail.com>
2022-07-18 14:45:29 +0900

    [NO-ISSUE] react-router-dom setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import MainPage from './pages/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
