32defeaec3e096fef7ebdffc9ed492c8750708b1
Pyosangwoo <psw3619@naver.com>
2022-07-28 22:02:16 +0900

    [SPT-01] refact. Header 리펙토링

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import HeaderBox from './styles';
+import * as styled from './styles';
 
 const Header = () => {
-  return <HeaderBox />;
+  return (
+    <styled.Container>
+      <styled.Logo />
+      <styled.RegistBox>
+        <styled.Login>로그인</styled.Login>/
+        <styled.SignUp>회원가입</styled.SignUp>
+        <styled.Loginimg />
+      </styled.RegistBox>
+    </styled.Container>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -3,21 +3,21 @@ import styled from 'styled-components';
 import logoblack from '../../imgs/logoblack.svg';
 import login from '../../imgs/login.svg';
 
-const Box = styled.div`
+export const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-around;
   align-items: center;
 `;
 
-const Logo = styled.img.attrs({
+export const Logo = styled.img.attrs({
   src: logoblack,
 })`
   width: 6.5vw;
   padding: 1vw 1vw;
 `;
 
-const RegistBox = styled.div`
+export const RegistBox = styled.div`
   display: flex;
   padding: 1vw;
   font-size: 1vw;
@@ -25,31 +25,19 @@ const RegistBox = styled.div`
   align-items: center;
 `;
 
-const Login = styled.div`
+export const Login = styled.div`
   cursor: pointer;
   padding: 1vw;
 `;
 
-const SignUp = styled.div`
+export const SignUp = styled.div`
   cursor: pointer;
   padding: 1vw;
 `;
 
-const Loginimg = styled.img.attrs({
+export const Loginimg = styled.img.attrs({
   src: login,
 })`
   width: 1.5vw;
   padding: 1vw;
 `;
-
-const HeaderBox = () => (
-  <Box>
-    <Logo />
-    <RegistBox>
-      <Login>로그인</Login>/<SignUp>회원가입</SignUp>
-      <Loginimg />
-    </RegistBox>
-  </Box>
-);
-
-export default HeaderBox;
