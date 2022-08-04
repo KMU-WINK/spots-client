2061c41d985ed191e76399f3c410fc055b95168f
Pyosangwoo <psw3619@naver.com>
2022-08-04 15:47:30 +0900

    [SPT-01] refact. styled-components refact

--- src/components/Footer/styles.jsx
+++ src/components/Footer/styles.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import logowhite from '../../imgs/logowhite.svg';
 
-const Box = styled.div`
+export const Container = styled.div`
   width: 100%;
   min-width: 1200px;
   display: flex;
@@ -10,7 +10,7 @@ const Box = styled.div`
   background-color: black;
 `;
 
-const BoxContainer = styled.div`
+export const BoxContainer = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
@@ -19,13 +19,13 @@ const BoxContainer = styled.div`
   max-width: 1920px;
 `;
 
-const Logo = styled.img.attrs({
+export const Logo = styled.img.attrs({
   src: logowhite,
 })`
   width: 123px;
 `;
 
-const Info = styled.div`
+export const Info = styled.div`
   justify-content: center;
   align-items: center;
   color: white;
@@ -35,7 +35,7 @@ const Info = styled.div`
   font-size: 14px;
 `;
 
-const Connact = styled.div`
+export const Connact = styled.div`
   justify-content: center;
   align-items: center;
   color: white;
@@ -49,29 +49,3 @@ const Connact = styled.div`
   text-decoration: underline;
   cursor: pointer;
 `;
-
-const FooterBox = () => (
-  <Box>
-    <BoxContainer>
-      <Info>
-        <Logo />
-        <br />
-        스포츠 팀원 매칭 서비스 Spot:s는 함께하는 즐거움을 바탕으로
-        <br />
-        건강한 스포츠 문화를 만들기 위해 고민합니다.
-        <br />
-        쉽고 빠르게 운동을 함께 할 친구를 구할 수 있도록 하며
-        <br />더 많은 사람이 더 즐겁게 운동할 수 있도록 최선을 다합니다.
-      </Info>
-      <Connact>
-        소셜미디어
-        <br />
-        매칭 찾기
-        <br />
-        매칭 올리기
-      </Connact>
-    </BoxContainer>
-  </Box>
-);
-
-export default FooterBox;
