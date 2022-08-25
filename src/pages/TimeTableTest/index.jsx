6f5c8911858200d6e030496fd4ac3944c8b63d30
j.myeong_ <jmyeong012@gmail.com>
2022-08-25 20:25:53 +0900

    feat. Timetable

---
+++ src/pages/TimeTableTest/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TimeTable from '../../components/TimeTable';
+
+export default function TimeTableTest() {
+  return (
+    <div style={{ width: '990px' }}>
+      <TimeTable />
+    </div>
+  );
+}
